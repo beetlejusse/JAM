8e0773c63184c999ceba788e066d6b859059fe74
vishalBelwal <vishalbelwal52@gmail.com>
2024-11-24 22:36:03 +0530

    middleware changes

--- server/src/middleware/auth.middleware.js
+++ server/src/middleware/auth.middleware.js
@@ -2,7 +2,7 @@ import {clerkClient} from '@clerk/express'
 
 //check user is authenticated or not
 export const protectRoute = async(req, res, next) => {
-    if(!req.auth?.userId){
+    if(!req.auth.userId){
         return res.status(401).json({
             message: "Unauthorized - you must be logged in"
         })
