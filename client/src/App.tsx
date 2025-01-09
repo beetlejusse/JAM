ee6f4a82e38a6a1a70c6138877e7d811d65380f7
vishalBelwal <vishalbelwal52@gmail.com>
2025-01-09 17:49:39 +0530

    routing all paths and whole apps ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,32 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/clerk-react";
-import { Button } from "./components/ui/button";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import AuthCallBackPage from "./pages/auth/AuthCallBackPage";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout";
+import { ChatPage } from "./pages/chat/ChatPage";
+import AlbumPage from "./pages/album/AlbumPage";
 
 function App() {
   return (
-    <header>
-      <SignedOut>
-        <SignInButton>
-          <Button>SignIn</Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+    <>
+      <Routes>
+        <Route
+          path="/sso-callback"
+          element={
+            <AuthenticateWithRedirectCallback
+              signUpForceRedirectUrl={"/auth"}
+            />
+          }
+        />
+        <Route path="/auth" element={<AuthCallBackPage />} />
+
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="/albums/:albumId" element={<AlbumPage />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
