c7565cabb355a2e01a30dd7529e9214ab9b1e6ca
vishalBelwal <vishalbelwal52@gmail.com>
2025-01-10 17:34:06 +0530

    topbar component ui for now

--- client/src/components/TopBar.tsx
+++ client/src/components/TopBar.tsx
@@ -1,11 +1,15 @@
-import { SignedIn, SignedOut, SignOutButton } from "@clerk/clerk-react"
+import {  SignedOut } from "@clerk/clerk-react"
 import { LayoutDashboardIcon } from "lucide-react"
 import { Link } from "react-router-dom"
 import { SignInOAuthButton } from "./SignInOAuthButton"
+import { useAuthStore } from "@/stores/useAuthStores"
+import { buttonVariants } from "./ui/button"
+import { cn } from "@/lib/utils"
 
 const TopBar = () => {
 
-    const isAdmin = false
+    const {isAdmin} = useAuthStore()
+    console.log({isAdmin})
 
   return (
     <div className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10">
@@ -14,15 +18,15 @@ const TopBar = () => {
         </div>
         <div className="flex items-center gap-4">
             {isAdmin && (
-                <Link to={"/admin"} >
+                <Link to={"/admin"} className={cn(
+                    buttonVariants({variant: "outline"})
+                )}>
                     <LayoutDashboardIcon className="size-4 mr-2" />
                     Admin Dashboard
                 </Link>
             )}
 
-            <SignedIn>
-                <SignOutButton />
-            </SignedIn>
+            
 
             <SignedOut>
                 {/* signin with google */}
