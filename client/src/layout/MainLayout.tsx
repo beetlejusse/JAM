f040cce6b907146feeb0aeb3c7be5886a54e2ab7
vishalBelwal <vishalbelwal52@gmail.com>
2025-01-26 20:36:16 +0530

    Implement responsive MainLayout with mobile detection and audio player components

--- client/src/layout/MainLayout.tsx
+++ client/src/layout/MainLayout.tsx
@@ -5,9 +5,21 @@ import {
 } from "@/components/ui/resizable";
 import { Outlet } from "react-router-dom";
 import { LeftSideBarComponent } from "./LayoutComponent/LeftSideBarComponent";
+import { FriendsActivityComponents } from "./LayoutComponent/FriendsActivityComponents";
+import AudioPlayer from "./LayoutComponent/AudioPlayer";
+import PlayBackControls from "./LayoutComponent/PlayBackControls";
+import { useEffect, useState } from "react";
 
 const MainLayout = () => {
-  const isMobile = false;
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth < 768)
+    }
+    checkMobile()
+    window.addEventListener("resize", checkMobile)
+    return () => window.removeEventListener("resize", checkMobile)
+  }, [])
 
   return (
     <div className="h-screen bg-black text-white flex flex-col">
@@ -15,6 +27,7 @@ const MainLayout = () => {
         direction="horizontal"
         className="flex-1 h-full overflow-hidden p-2"
       >
+        <AudioPlayer />
         {/* left side bar component */}
         <ResizablePanel
           defaultSize={20}
@@ -29,18 +42,26 @@ const MainLayout = () => {
         <ResizablePanel defaultSize={isMobile ? 80 : 60}>
           <Outlet />
         </ResizablePanel>
-        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+        {
+          !isMobile && (
+            <>
+              <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
 
-        {/* right side */}
-        <ResizablePanel
-          defaultSize={20}
-          minSize={0}
-          maxSize={25}
-          collapsedSize={0}
-        >
-          friend activity component
-        </ResizablePanel>
+              {/* right side */}
+              <ResizablePanel
+                defaultSize={20}
+                minSize={0}
+                maxSize={25}
+                collapsedSize={0}
+              >
+                <FriendsActivityComponents />
+              </ResizablePanel>
+            </>
+          )
+        }
       </ResizablePanelGroup>
+
+      <PlayBackControls />
     </div>
   );
 };
