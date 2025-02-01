96c149f1f63ff2dbcf95da65e233a235e5c85b58
vishalBelwal <vishalbelwal52@gmail.com>
2025-02-01 17:40:54 +0530

    chore: Add endpoint to retrieve user messages by ID

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getAllUser } from "../controllers/user.controller.js";
+import { getAllUser, getAllUserMessages } from "../controllers/user.controller.js";
 
 const router = Router()
 
 router.get("/", protectRoute, getAllUser)
+router.get("/messages/:userId", protectRoute, getAllUserMessages)
 
 export default router;
