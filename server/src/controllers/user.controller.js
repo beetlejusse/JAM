0bce05f3b298a60a15788a656b05eb4b3a8ae06f
vishalBelwal <vishalbelwal52@gmail.com>
2025-02-01 17:41:04 +0530

    chore: Add endpoint to retrieve user messages by ID

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import {User} from "../models/user.model.js"
+import { Message } from "../models/message.model.js"
 
 export const getAllUser = async(req, res, next) => {
     try {
@@ -10,5 +11,28 @@ export const getAllUser = async(req, res, next) => {
     } catch (error) {
         console.log("Error fetching user at user controller", error)
         next(error)
+    }
+}
+
+export const getAllUserMessages = async(req, res, next) => {
+    try {
+        const myId = req.auth.userId
+        const {userId} = req.params
+
+        const messages = await Message.find({
+            $or: [
+                {senderId: myId, receiverId: userId},
+                {senderId: userId, receiverId: myId}
+            ]
+        }).sort({createdAt: 1})
+
+        return res.status(200).json({
+            message: "Messages fetched successfully",
+            messages
+        })
+    } catch (error) {
+        console.log("Error fetching user messages at user controller", error)
+        next(error)
+        
     }
 }
