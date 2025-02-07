4070a6edf528af8aa8ac2d625e2179326779f481
vishalBelwal <vishalbelwal52@gmail.com>
2025-02-08 00:13:05 +0530

    chore: Update protectRoute middleware to handle missing userId in req.auth

--- server/src/middleware/auth.middleware.js
+++ server/src/middleware/auth.middleware.js
@@ -2,12 +2,11 @@ import {clerkClient} from '@clerk/express'
 
 //check user is authenticated or not
 export const protectRoute = async(req, res, next) => {
-    if(!req.auth.userId){
+    if(!req.auth.userId ){
         return res.status(401).json({
             message: "Unauthorized - you must be logged in"
         })
     }
-
     next()
 }
 
